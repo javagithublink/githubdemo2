b0b6d339d728f29003e2915671378ef667d27918
javagithublink <javagithublink@gmail.com>
2021-11-04 19:28:10 +0000

    3rd Commit. changes made to GitLocalRepoExample

--- src/GitLocalRepoExample.java
+++ src/GitLocalRepoExample.java
@@ -3,5 +3,10 @@ public class GitLocalRepoExample {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("2nd Message");
+
+        //changes made to file
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
     }
 }
